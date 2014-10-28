619e5656c3fa828d203e33fa217f62f0ce25aaf9
Oswaldo Ortiz <oswaldo.ortiz.sv@gmail.com>
2014-10-28 11:21:18 -0600

    instalacion y creacion de la pagina principal

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+admin:$1$cxhCquui$VCfrx.4cZmfroasp84PcC1:Oswaldo Ortiz:oswaldo.ortiz.sv@gmail.com:admin,user
